861db75ce02487c1698e3d478ec5eb86e80f84d1
Alvaro Salazar <ah.salazar@uniandes.edu.co>
2024-08-28 11:38:31 -0500

    application.properties agregado y restcontroller con listar paises

--- src/main/java/co/edu/uceva/countryservice/controller/CountryRestController.java
+++ src/main/java/co/edu/uceva/countryservice/controller/CountryRestController.java
@@ -1,10 +1,22 @@
 package co.edu.uceva.countryservice.controller;
 
+import co.edu.uceva.countryservice.model.entities.Pais;
+import co.edu.uceva.countryservice.model.service.PaisServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController("/api/v1/country-service")
 public class CountryRestController {
 
+    @Autowired
+    private PaisServiceImpl paisService;
 
+    @GetMapping("/paises")
+    public List<Pais> listar() {
+        return this.paisService.listar();
+    }
 
 }
--- src/main/java/co/edu/uceva/countryservice/model/service/PaisServiceImpl.java
+++ src/main/java/co/edu/uceva/countryservice/model/service/PaisServiceImpl.java
@@ -4,6 +4,7 @@ import co.edu.uceva.countryservice.model.dao.PaisDao;
 import co.edu.uceva.countryservice.model.entities.Pais;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -11,10 +12,11 @@ import java.util.List;
 /**
  * Esta interfaz define los metodos que se van a implementar en la clase PaisServiceImpl
  */
-@Component
+@Service
 public class PaisServiceImpl implements IPaisService {
     @Autowired
     PaisDao paisDao;
+
     /**
      * Este metodo se encarga de listar los paises
      * @return retorna una lista de paises
